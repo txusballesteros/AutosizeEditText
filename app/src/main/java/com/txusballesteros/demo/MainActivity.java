b68f730a5aaece748b8ed25aee3b2f79bb5fb243
Txus Ballesteros <txus.ballesteros@redbooth.com>
2015-10-16 09:14:48 +0200

    Cleaning Dependencies

--- app/src/main/java/com/txusballesteros/demo/MainActivity.java
+++ app/src/main/java/com/txusballesteros/demo/MainActivity.java
@@ -25,11 +25,7 @@
 package com.txusballesteros.demo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
